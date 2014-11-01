6c31bdff68610019c464a5c6c390885b61444546
bhargava <bhargava@bhargava-ThinkPad-T42.(none)>
2014-11-01 19:49:21 -0400

    work

--- predictor.h
+++ predictor.h
@@ -18,14 +18,16 @@ class PREDICTOR
     typedef uint32_t address_t;
 
   private:
-    static const int num_DNF_terms = 4; 
+    static const int num_DNF_terms = 6;
+	 
     struct branch_info_t
     {
-	std::vector <int> DNF;   
-	typedef uint8_t last_N_pred; 
-	typedef uint8_t iteration; //determine if just beginning building the branch 
-	typedef uint8_t num_pred_T_but_NT;
-    } branch_info; 
+		std::vector < std::vector<int> > DNF;
+		int num_terms;    
+		int last_N_pred; 
+		int iteration; //determine if just beginning building the branch 
+		int num_pred_T_but_NT;
+    };
     typedef uint32_t history_t;
     typedef uint8_t counter_t;
     static const int BHR_LENGTH = 4; //global BHR 
@@ -34,11 +36,11 @@ class PREDICTOR
     static const std::size_t PHT_INDEX_MASK = (PHT_SIZE - 1);
     static const counter_t PHT_INIT = /* weakly taken */ 2;
 
-    history_t bhr;                // 15 bits
+    history_t bhr;                // 4 bits
     std::vector<counter_t> pht;   // 64K bits
     std::map<address_t, branch_info_t> branch_table; 
     
-    void update_bhr(bool taken) { bhr >>= 1; if (taken) bhr |= BHR_MSB; }
+    void update_bhr(bool taken) { bhr >>= 1; if (taken) bhr |= BHR_MSB; }//right shift so most recent is MSB 
     static std::size_t pht_index(address_t pc, history_t bhr) 
         { return (static_cast<std::size_t>(pc ^ bhr) & PHT_INDEX_MASK); }
     static bool counter_msb(/* 2-bit counter */ counter_t cnt) { return (cnt >= 2); }
@@ -61,18 +63,77 @@ class PREDICTOR
     bool get_prediction(const branch_record_c* br, const op_state_c* os)
         {
             bool prediction = false;
-            if (/* conditional branch */ br->is_conditional) {
+			
+            if (/* conditional branch */ br->is_conditional) 
+			{
+				/*
                 address_t pc = br->instruction_addr;
                 std::size_t index = pht_index(pc, bhr);
                 counter_t cnt = pht[index];
                 prediction = counter_msb(cnt);
+				*/
+				address_t pc = br->instruction_addr; 
+				if (branch_table.find(pc) == branch_table.end())
+				{
+					//branch does not exist in map
+					branch_info_t branch_info;  
+					branch_info.DNF.resize(num_DNF_terms); 
+					for (int i = 0; i < num_DNF_terms; i++)
+					{
+						(branch_info.DNF[i]).resize(BHR_LENGTH); 
+					} 
+					branch_info.num_terms = 1; 
+					branch_info.last_N_pred = 0;  
+					branch_info.iteration = 0;  
+					branch_info.num_pred_T_but_NT = 0; 
+					branch_table[pc] = branch_info; //insert into map  
+				}
+				else
+				{
+					//branch exists in map
+					int result = 1;  
+					for (int i = 0; i < branch_table[pc].num_terms; i++)
+					{
+						for (int j = 0; j < BHR_LENGTH; j++)
+						{
+							if (branch_table[pc].DNF[i][j] == 1)
+							{
+								result = result & ((bhr >> (BHR_LENGTH-j)) & 1);  
+								if (result == 0) {
+									break; 
+								}
+							}
+							else if (branch_table[pc].DNF[i][j] == 0)
+							{
+								result = result & ~((bhr >> (BHR_LENGTH-j)) & 1);
+ 								if (result == 0) {
+                                    break;
+                                }
+ 
+							}		
+							else //-1 
+							{
+								//do nothing 
+							}
+
+							if (j == BHR_LENGTH-1)
+							{
+								if (result == 1)
+								{
+									prediction = true; 
+									goto end; 
+								}
+							}
+						}
+					}
+				}
+				
             }
-	    std::cout << "pht contains:";
-	    for (unsigned i=0; i < pht.size(); i++)
-		std::cout << ' ' << (int)pht.at(i);
-	    std::cout << '\n'; 
-	    //printf("bhr: %d \n",bhr);  
-	    return prediction;   // true for taken, false for not taken
+			end:
+		
+			
+	    	//printf("bhr: %d \n",bhr);  
+	    	return prediction;   // true for taken, false for not taken
         }
 
     // Update the predictor after a prediction has been made.  This should accept
